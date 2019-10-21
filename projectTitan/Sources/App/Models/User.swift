35925f4f8752b9c379db2b36b7d2d49518b22c00
Tyler Phillips <thetylerjp@icloud.com>
2019-10-21 15:31:32 -0400

    Initial

---
+++ projectTitan/Sources/App/Models/User.swift
@@ -0,0 +1,15 @@
+import FluentPostgreSQL
+import Vapor
+
+
+final class User: PostgreSQLModel {
+  var id: Int?
+  var username: String
+    init(id: Int? = nil, username: String) {
+        self.id = id
+        self.username = username
+  }
+}
+
+extension User: Content {}
+extension User: Migration {}
---
+++ projectTitan/Sources/App/configure.swift
@@ -0,0 +1,34 @@
+import Vapor
+import Leaf
+import FluentPostgreSQL
+
+/// Called before your application initializes.
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+) throws {
+
+    /// Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+
+    let leafProvider = LeafProvider()
+    try services.register(leafProvider)
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
+
+    try services.register(FluentPostgreSQLProvider())
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
+    let postgresqlConfig = PostgreSQLDatabaseConfig(
+        hostname: "127.0.0.1",
+        port: 5432,
+        username: "martinlasek",
+        database: "mycooldb",
+        password: nil
+      )
+    services.register(postgresqlConfig)
+    var migrations = MigrationConfig()
+    migrations.add(model: User.self, database: .psql)
+    services.register(migrations)
+}
